f58d9db578e1a029fc7c48da47451ee9b632238e
Carlos <carlosveizaga.jcvc@gmail.com>
2018-09-04 09:04:19 -0400

    Creando menu de clientes

--- src/internationalization/Mensajes.java
+++ src/internationalization/Mensajes.java
@@ -1,22 +1,21 @@
-/*    */ package internationalization;
-/*    */ 
-/*    */ import java.util.MissingResourceException;
-/*    */ import java.util.ResourceBundle;
-/*    */ 
-/*    */ public class Mensajes
-/*    */ {
-/*  8 */   private static ResourceBundle recursos = ResourceBundle.getBundle("internationalization.Mensajes");
-/*    */ 
-/*    */   public static String getString(String key)
-/*    */   {
-/*    */     try
-/*    */     {
-/* 17 */       return recursos.getString(key);
-/*    */     } catch (MissingResourceException e) {
-/*    */     }
-/* 20 */     return '!' + key + '!';
-/*    */   }
-/*    */ }
+package internationalization;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Mensajes
+{
+    private static ResourceBundle recursos = ResourceBundle.getBundle("internationalization.Mensajes");
+
+    public static String getString(String key)
+    {
+        try{
+          return recursos.getString(key);
+        } catch (MissingResourceException e) {
+            }
+         return '!' + key + '!';
+    }
+}
 
 /* Location:           /media/sda1/contaes4/contaes4.jar
  * Qualified Name:     internationalization.Mensajes
